e9bdb49a3205b376ed141cccfd2b0012b69cbd39
elshammar <kanyindamampova@gmail.com>
2023-12-26 19:20:34 +0400

    diki

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,11 @@
 <div>
-    <div>
-        <livewire:pages.nav>
-        <livewire:pages.header>
+    <div class="" >
+        <div>
+            <livewire:pages.nav>
+        </div>
+        <div>
+            <livewire:pages.header>
+        </div>
     </div>
     <livewire:pages.about>
     <livewire:pages.actualite>
